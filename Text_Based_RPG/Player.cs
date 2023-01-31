55c4eb34a0fbcd71e50255552c5b6c965638af97
Evan MacDonald <113189803+Evan-Prime@users.noreply.github.com>
2023-01-31 19:51:20 -0400

    Initial commit

---
+++ Text_Based_RPG/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Based_RPG
+{
+    internal class Player
+    {
+        static int playerPosx; // play position
+        static int playerPosy; // play position
+
+        static void Update()
+        {
+            // read user input
+            ConsoleKeyInfo input;
+            input = Console.ReadKey(true);
+
+            if (input.Key == ConsoleKey.W || input.Key == ConsoleKey.UpArrow)
+            {
+                playerPosy--;
+            }
+            if (input.Key == ConsoleKey.S || input.Key == ConsoleKey.DownArrow)
+            {
+                playerPosy++;
+            }
+            if (input.Key == ConsoleKey.D || input.Key == ConsoleKey.RightArrow)
+            {
+                playerPosx++;
+            }
+            if (input.Key == ConsoleKey.A || input.Key == ConsoleKey.LeftArrow)
+            {
+                playerPosx--;
+            }
+            if (input.Key == ConsoleKey.Escape)
+            {
+                gameOver = true;
+            }
+
+        }
+
+        static void Draw()
+        {
+            Console.Clear();
+
+            Console.SetCursorPosition(playerPosx, playerPosy);
+            Console.Write("☺");
+        }
+    }
+}
---
+++ Text_Based_RPG/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Based_RPG
+{
+    internal class Program
+    {
+        public bool gameOver; // true or false
+        static Map map = new Map();
+        static Player player = new Player();
+
+        static void Main(string[] args)
+        {
+            gameOver = false;
+            map.DrawMap(1);
+            while (gameOver == false)
+            {
+                player.Update();
+                player.Draw();
+                if (input.Key == ConsoleKey.Escape)
+                {
+                    gameOver = true;
+                }
+            }
+            Console.ReadKey(true);
+        }
+
+    }
+}
